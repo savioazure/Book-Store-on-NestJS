280e30a5407380fd467400d76977ffb0051ad3b6
Savio Kerber <savioazure@gmail.com>
2020-10-03 06:08:41 -0700

    .env, database module, config keys

---
+++ src/database/database.service.ts
@@ -0,0 +1,23 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { ConnectionOptions } from 'typeorm';
+import { Configuration } from '../config/config.keys';
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    async useFactory(config: ConfigService) {
+      return {
+        ssl: true,
+        type: 'postgres' as 'postgres',
+        host: config.get(Configuration.HOST),
+        username: config.get(Configuration.USERNAME),
+        password: config.get(Configuration.PASSWORD),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../**/*.entity{.ts,.js}'],
+      } as ConnectionOptions;
+    },
+  }),
+];
